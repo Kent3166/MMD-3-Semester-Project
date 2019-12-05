c38684cf052b3a448ff54f32abae78546955e9ec
Kent Madsen <kent3166@easv365.dk>
2019-12-05 23:23:26 +0100

    a

--- page-about.php
+++ page-about.php
@@ -5,14 +5,54 @@
     <main> 
     
         <!-- About Template --> 
+        <?php 
+            while( have_posts() ):
+                    the_post();
+        ?>
+
         <section> 
-        </section>
         
-        <section> 
+            <!--  -->
+         
+                <h1> 
+                    <?php the_title(); ?> 
+                </h1>
+                
+                <div>
+                    <?php the_content(); ?> 
+                </div>
+
         </section>
+
         
-        <section> 
+        <?php 
+            $children = get_pages( array( 'sort_order'=>'desc', 'parent' => $post->ID ) );
+
+            foreach( $children as $post ):
+                setup_postdata( $post );
+        ?>
+
+        <?php if( get_field( 'section-overlay' ) ): ?>
+
+        <section class="<?php the_field( 'section-overlay' ); ?>"> 
+            <h1> 
+                <?php the_title(); ?> 
+            </h1>
+        
+            <p> 
+                <?php the_content(); ?>
+            </p>
         </section>
+
+        <?php endif; ?>
+        
+        <?php 
+            endforeach;
+            wp_reset_postdata();
+        ?>
+
+        <?php endwhile; ?>     
+
     </main>
         
 <?php
