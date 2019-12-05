3169c982ca200fbf7ac4ced39f34962221fc0aa3
Kent Madsen <kent3166@easv365.dk>
2019-12-05 13:47:44 +0100

    404 content added

--- 404.php
+++ 404.php
@@ -2,7 +2,30 @@
     get_header(); 
 ?>
 
-    <main> 
+    <main>
+        <div>
+            <div> 
+                <p> 404 </p>
+                <p> Page not found </p>
+            </div>
+            
+            <div> 
+                <p> Looks like the page you requested doesn't exist anymore. Reasons could be it, has changed location, too old or never existed. </p>
+            </div>
+
+            <div> 
+                <div> 
+                    <!-- Back -->
+                </div>
+
+                <div> 
+                    <!-- Home -->
+                </div>
+                <div>
+                    <!-- Sitemap -->
+                </div>
+            </div>
+        </div> 
 
     </main>
         
